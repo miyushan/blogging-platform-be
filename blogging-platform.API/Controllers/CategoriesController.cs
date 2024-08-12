f88067a647c6c05c24cfdaf580021e16f210c7e6
miyushan <miyushanrodrigo@gmail.com>
2024-08-12 16:16:05 +0530

    feat: implement get single category and create category endpoints

--- blogging-platform.API/Controllers/CategoriesController.cs
+++ blogging-platform.API/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using blogging_platform.API.Models.Domain;
+using blogging_platform.API.Models.DTO;
 using blogging_platform.API.Data;
 
 namespace blogging_platform.API.Controllers
@@ -17,28 +18,73 @@ namespace blogging_platform.API.Controllers
         }
 
         // GET: All categories
-        // GET: api/Categories
+        // GET: baseUrl/api/Categories
         [HttpGet]
         public IActionResult GetAll()
         {
+            // Get data from database
             var categories = dbContext.Categories.ToList();
 
+            // Map domain models to DTOs
+            var categoryDto = new List<CategoryDto>();
+            
+            foreach (var category in categories)
+            {   
+                categoryDto.Add(new CategoryDto(){
+                    Id = category.Id,
+                    Name = category.Name
+                });
+            }
+
+            // Return DTOs
             return Ok(categories);
         }
 
         // GET: Single category     
-        // GET: api/Categories/:id
+        // GET: baseUrl/api/Categories/:id
         [HttpGet]
         [Route("{id:Guid}")]
         public IActionResult GetById([FromRoute] Guid id)
         {
+            // Get data from database
             var category = dbContext.Categories.Find(id);
             if (category == null)
             {
                 return NotFound();
             }
 
-            return Ok(category);
+            // Map domain models to DTOs
+            var categoryDto = new CategoryDto{
+                Id = category.Id,
+                Name = category.Name
+            };
+            return Ok(categoryDto);
         }
+    
+        // POST: Add new category
+        // POST: baseUrl/api/Categories
+        [HttpPost]
+        public IActionResult Add([FromBody] AddCategoryReqDto category)
+        {
+            // Map DTO to domain model
+            var newCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = category.Name
+            };
+
+            // Add domain model to database
+            dbContext.Categories.Add(newCategory);
+            dbContext.SaveChanges();
+
+            // Return DTO
+            var newCategoryDto = new CategoryDto{
+                Id = newCategory.Id,
+                Name = newCategory.Name
+            };
+
+            return CreatedAtAction(nameof(GetById), new {id=newCategoryDto.Id}, newCategoryDto);
+        }
+    
     }
 }
---
+++ blogging-platform.API/Models/DTO/AddCategoryReqDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogging_platform.API.Models.DTO
+{
+    public class AddCategoryReqDto
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
