a076fc3a4defba7eb907246784dc9a876dd473f5
miyushan <miyushanrodrigo@gmail.com>
2024-08-14 00:23:50 +0530

    feat: develop post related endpoints

--- blogging-platform.API/Controllers/PostsController.cs
+++ blogging-platform.API/Controllers/PostsController.cs
@@ -12,27 +12,45 @@ namespace blogging_platform.API.Controllers
     [ApiController]
     public class PostsController : ControllerBase
     {
-        private readonly BloggingPlatformDbContext dbContext;
+        private readonly BloggingPlatformDbContext _dbContext;
 
         public PostsController(BloggingPlatformDbContext dbContext)
         {
-            this.dbContext = dbContext;
+            _dbContext = dbContext;
         }
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            var posts = dbContext.Posts.ToList();
+            var posts = _dbContext.Posts.ToList();
 
             var postsDto = new List<GetPostResDto>();
             foreach (var post in posts)
             {   
+                var author = _dbContext.Users.Find(post.UserId);
+                var category = _dbContext.Categories.Find(post.CategoryId);
+
+                if(author == null || category == null)
+                {
+                    continue;
+                }
+
                 postsDto.Add(new GetPostResDto(){
-                    Id = post.PostId,
+                    PostId = post.PostId,
                     Title = post.Title,
                     Content = post.Content,
-                    UserId = post.UserId,
-                    CategoryId = post.CategoryId
+                    Author =  new GetAuthorDto()
+                    {
+                        Id = author.UserId,
+                        FirstName = author.FirstName,
+                        LastName = author.LastName,
+
+                    },
+                    Category = new GetCategoryDto()
+                    {
+                        Id = category.CategoryId,
+                        Name = category.Name    
+                    }
                 });
             }
             return Ok(postsDto);
@@ -42,18 +60,29 @@ namespace blogging_platform.API.Controllers
         [Route("{id:Guid}")]
         public IActionResult GetById([FromRoute] Guid id)
         {
-            var post = dbContext.Posts.Find(id);
-            if (post == null)
+            var post = _dbContext.Posts.Find(id);
+            var author = _dbContext.Users.Find(post?.UserId);
+            var category = _dbContext.Categories.Find(post?.CategoryId);
+            if (post == null || author == null || category == null)
             {
                 return NotFound();
             }
 
             var postDto = new GetPostResDto{
-                Id = post.PostId,
+                PostId = post.PostId,
                 Title = post.Title,
                 Content = post.Content,
-                UserId = post.UserId,
-                CategoryId = post.CategoryId
+                Author = new GetAuthorDto()
+                {
+                    Id = author.UserId,
+                    FirstName = author.FirstName,
+                    LastName = author.LastName,
+                },
+                Category = new GetCategoryDto()
+                {
+                    Id = category.CategoryId,
+                    Name = category.Name
+                }
             };
             return Ok(postDto);
         }
@@ -70,6 +99,11 @@ namespace blogging_platform.API.Controllers
                 return BadRequest(results.Errors);
             }
 
+            var author = _dbContext.Users.Find(post.UserId);
+            var category = _dbContext.Categories.Find(post.CategoryId);
+            if (author == null || category == null) {
+                return NotFound();
+            }
             var newPost = new Post
             {
                 PostId = Guid.NewGuid(),
@@ -79,18 +113,27 @@ namespace blogging_platform.API.Controllers
                 CategoryId = post.CategoryId
             };
 
-            dbContext.Posts.Add(newPost);
-            dbContext.SaveChanges();
+            _dbContext.Posts.Add(newPost);
+            _dbContext.SaveChanges();
 
             var newPostDto = new GetPostResDto{
-                Id = newPost.PostId,
+                PostId = newPost.PostId,
                 Title = newPost.Title,
                 Content = newPost.Content,
-                UserId = newPost.UserId,
-                CategoryId = newPost.CategoryId
+                Author = new GetAuthorDto()
+                {
+                    Id = author.UserId,
+                    FirstName = author.FirstName,
+                    LastName = author.LastName,
+                },
+                Category = new GetCategoryDto()
+                {
+                    Id = category.CategoryId,
+                    Name = category.Name
+                }
             };
 
-            return CreatedAtAction(nameof(GetById), new {id=newPostDto.Id}, newPostDto);
+            return CreatedAtAction(nameof(GetById), new {id=newPostDto.PostId}, newPostDto);
         }
     
     }
--- blogging-platform.API/Models/DTO/GetPostResDto.cs
+++ blogging-platform.API/Models/DTO/GetPostResDto.cs
@@ -1,12 +1,27 @@
 
+
 namespace blogging_platform.API.Models.DTO
 {
-    public class GetPostResDto
+    public class GetAuthorDto
     {
         public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+    }
+
+    public class GetCategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class GetPostResDto
+    {
+        public Guid PostId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
-        public Guid UserId { get; set; }
-        public Guid CategoryId { get; set; }
+        public GetAuthorDto Author { get; set; } = null!;
+        public GetCategoryDto Category { get; set; } = null!;
     }
 }
